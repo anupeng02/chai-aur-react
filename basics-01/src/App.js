17e33de7ea3fb9092468837dfbf39d687b3df7c4
anupeng02 <anupsofteng@gmail.com>
2024-08-28 09:57:42 +0530

    commit done

--- basics-01/src/App.js
+++ basics-01/src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <FetchApi/>
-      <RegistrationForm/>
+      {/* <FetchApi/>
+      <RegistrationForm/> */}
     </div>
   );
 }
