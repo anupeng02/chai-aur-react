3aa2222d5ac77ef2b04d18712bc80ee0b9365af0
anupeng02 <anupsofteng@gmail.com>
2024-08-28 13:19:58 +0530

    02-counter filoe commited

--- 02-counter/src/App.js
+++ 02-counter/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {useState} from "react"
 function App() {
+
+  let [counter, setCounter] = useState(0)
+    console.log(counter)
+
+  function incrementFunction(){
+   
+   if(counter<20){
+    setCounter(counter=>counter+1)
+   }
+  }
+
+  function decrementFunction(){
+    //  setCounter(counter=>counter-1)
+    if(counter>0){
+      setCounter(counter=>counter-1)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+         <h1>lets make counter</h1>
+
+         <h1>{counter}</h1>
+         
+          <button onClick={incrementFunction}>increment</button> <br/>
+          <button onClick={decrementFunction}>Decrement</button> <br/>
+         
     </div>
   );
 }
